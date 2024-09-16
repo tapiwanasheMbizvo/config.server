3ab9d72d68c1dbfd3c783fc1fde20f576d2117cd
tapiwa <tapiwambizvo@gmail.com>
2024-09-16 14:07:27 +0200

    dockerise application

---
+++ src/main/resources/db/migration/V2024.09.15.1428__hilt_backend-main-properties.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO properties(APPLICATION, PROFILE, LABEL, PROP_KEY, VALUE)
+VALUES
+('hilt-backend', 'dev', 'latest', 'HILT_BACK_END_DB_URL', 'jdbc:postgresql://localhost:5543/hilt_main_db'),
+('hilt-backend', 'dev', 'latest', 'HILT_DB_USER', 'hilt_app_user'),
+('hilt-backend', 'dev', 'latest', 'HILT_DB_PSWD', 'hilt_app_user')
